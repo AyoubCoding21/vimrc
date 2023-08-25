d4e68bff9040a5515a4958d0466564a0012661aa
Ayoub Coding <ayoubprogramming95@gmail.com>
2023-08-25 16:53:13 +0100

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -2,9 +2,11 @@ echo -e "\e[1;33m${bold}Installing packages...\e[0m";
 if [ $OSTYPE == "linux-androideabi" ]
 then
 	apt install vim -y
+ 	echo -e "\e[1;33m${bold}Copying files...\e[0m";
+	cp .vimrc ~/ -rf
 else
 	sudo apt install vim -y
+ 	echo -e "\e[1;33m${bold}Copying files...\e[0m";
+	cp .linux-vimrc ~/.vimrc -rf
 fi
-echo -e "\e[1;33m${bold}Copying files...\e[0m";
-cp .vimrc ~/ -rf
 echo -e "\e[1;32m${bold}NVix is installed... Enjoy :)\e[0m";
